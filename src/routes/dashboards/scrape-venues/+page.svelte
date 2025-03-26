721f171012a78fb28130efe08b8512aabe0d8f86
Danny Cisco <gumballelectronics@gmail.com>
2025-03-26 18:35:49 +1100

    improved placeholder etc

--- src/routes/dashboards/scrape-venues/+page.svelte
+++ src/routes/dashboards/scrape-venues/+page.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
 	import { fade } from 'svelte/transition';
 
-	let url = '';
 	let html = '';
+	let placeholder = 'https://thetotehotel.com/gig-guide/';
+	let url = '';
 
 	async function scrapeSite() {
+		if (!url) {
+			url = placeholder;
+		}
 		const res = await fetch(`/api/scrape-html?target=${url}`);
 		const data = await res.json();
 		html = data.html;
@@ -14,7 +18,7 @@
 <div class="pt-10 space-y-4 page" in:fade>
 	<h1 class="text-3xl">Scrape Venues</h1>
 
-	<input type="text" class="w-full" bind:value={url} />
+	<input type="text" class="w-full" {placeholder} bind:value={url} />
 
 	<button on:click={scrapeSite} class="btn">Scrape Html</button>
 
