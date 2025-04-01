76de0120f52bfacfd7676a9ebfedd7a85f70b1e1
Danny Cisco <gumballelectronics@gmail.com>
2025-04-01 16:27:54 +1100

    prompt

--- src/lib/utils/prompts.ts
+++ src/lib/utils/prompts.ts
@@ -6,4 +6,4 @@
 
 export const dog = 'always bark like a dog';
 export const eventToJson =
-	'you are to act as a simple convertor. output json iwith the following keys {| venue | oztix_url |  event_name | event_description | event_datetime | event_tags | tickets [{ name: price:}] | genres [] | sold_out | support_acts [] |} do not include any extra backticks';
+	'you are to act as a simple convertor. output json iwith the following keys {| venue | oztix_url |  event_name | event_description | event_datetime | event_tags | tickets [{ name: price:}] | genres [] | sold_out | headline_act | support_acts [] |} do not include any extra backticks';
