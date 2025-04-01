2e33bcd7ae730d9f6ce8b6012b08dde44b41bd78
Danny Cisco <gumballelectronics@gmail.com>
2025-04-01 16:46:25 +1100

    added the pacman loading spinner

---
+++ src/lib/components/loadingSpinners/PacMan.svelte
@@ -0,0 +1,3 @@
+<div class="row">
+	loading <img src="/pac-man.svg" alt="" class=" size-10" transition:slide />
+</div>
--- src/routes/venues/the-gem/+page.svelte
+++ src/routes/venues/the-gem/+page.svelte
@@ -1,12 +1,16 @@
 <script>
+	import PacMan from '$lib/components/loadingSpinners/PacMan.svelte';
 	let venueName = 'The Gem';
 	let readOut = 'Ready to begin';
+	let loading = false;
 </script>
 
 <div class="space-y-4 page">
 	<h1 class="text-5xl font-bold text-center">
 		{venueName}
 	</h1>
-	<div class="w-full p-10 text-center bg-white/5">{readOut}</div>
+	<div class="flex flex-col items-center w-full p-10 text-center bg-white/5">
+		{readOut}{#if loading}<PacMan />{/if}
+	</div>
 	<button class="w-full btn">Get Links</button>
 </div>
