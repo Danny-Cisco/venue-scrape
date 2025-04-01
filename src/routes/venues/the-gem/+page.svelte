242deb56f4c722bf7a30c8a7d1e61074093440bb
Danny Cisco <gumballelectronics@gmail.com>
2025-04-01 16:38:01 +1100

    created Gem page

---
+++ src/routes/venues/the-gem/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	let venueName = 'The Gem';
+	let readOut = 'Ready to begin';
+</script>
+
+<div class="space-y-4 page">
+	<h1 class="text-5xl font-bold text-center">
+		{venueName}
+	</h1>
+	<div class="w-full p-10 text-center bg-white/5">{readOut}</div>
+	<button class="w-full btn">Get Links</button>
+</div>
