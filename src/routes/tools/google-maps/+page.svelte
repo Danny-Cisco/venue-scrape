28874d61ec17d31354e211f913cb7fcd963d64ef
Danny Cisco <gumballelectronics@gmail.com>
2025-05-12 18:35:07 +1000

    demo map works

---
+++ src/routes/tools/google-maps/+page.svelte
@@ -0,0 +1,48 @@
+<script>
+	import { onMount } from 'svelte';
+	import { PUBLIC_GOOGLE_API_KEY } from '$env/static/public';
+
+	let mapDiv;
+
+	const loadGoogleMapsScript = () => {
+		return new Promise((resolve, reject) => {
+			if (window.google && window.google.maps) {
+				resolve(window.google.maps);
+				return;
+			}
+			const script = document.createElement('script');
+			script.src = `https://maps.googleapis.com/maps/api/js?key=${PUBLIC_GOOGLE_API_KEY}`;
+			script.async = true;
+			script.defer = true;
+			script.onload = () => resolve(window.google.maps);
+			script.onerror = reject;
+			document.head.appendChild(script);
+		});
+	};
+
+	onMount(async () => {
+		const googleMaps = await loadGoogleMapsScript();
+
+		const brunswickCoords = { lat: -37.7706, lng: 144.9614 };
+
+		const map = new googleMaps.Map(mapDiv, {
+			center: brunswickCoords,
+			zoom: 15
+		});
+
+		new googleMaps.Marker({
+			position: brunswickCoords,
+			map,
+			title: 'Brunswick, Melbourne VIC'
+		});
+	});
+</script>
+
+<div bind:this={mapDiv} id="map"></div>
+
+<style>
+	#map {
+		width: 100%;
+		height: 100vh;
+	}
+</style>
