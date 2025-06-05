f953d5845af4f0a2ec580fdc4af902bb8ffb8c95
Danny Cisco <gumballelectronics@gmail.com>
2025-06-05 19:20:23 +1000

    hide non instagram

--- src/lib/components/modals/GigModal.svelte
+++ src/lib/components/modals/GigModal.svelte
@@ -172,67 +172,68 @@
 								</span></a
 							>
 						{:else}
-							no instagram account
+							<div class="text-xs">No instagram</div>
 						{/if}
 					</div>
+					{#if bandObject.instagram}
+						<!-- ⭐ Star Rating -->
+						<div class="flex ml-1">
+							{#each Array(5) as _, i}
+								{#if i < getStarCount(bandObject.instagram?.followersCount)}
+									<!-- Filled -->
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="#FACC15"
+										stroke="#FA9910"
+										viewBox="0 0 24 24"
+										stroke-width="1.5"
+										class="size-5"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+										/>
+									</svg>
+								{:else}
+									<!-- Outline -->
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke="#FAeeaa"
+										stroke-width="1.5"
+										class="size-5"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+										/>
+									</svg>
+								{/if}
+							{/each}
+						</div>
 
-					<!-- ⭐ Star Rating -->
-					<div class="flex ml-1">
-						{#each Array(5) as _, i}
-							{#if i < getStarCount(bandObject.instagram?.followersCount)}
-								<!-- Filled -->
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="#FACC15"
-									stroke="#FA9910"
-									viewBox="0 0 24 24"
-									stroke-width="1.5"
-									class="size-5"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
-									/>
-								</svg>
-							{:else}
-								<!-- Outline -->
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke="#FAeeaa"
-									stroke-width="1.5"
-									class="size-5"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
-									/>
-								</svg>
-							{/if}
-						{/each}
-					</div>
-
-					<!-- 🌐 External Link -->
-					<a href={bandObject.instagram?.externalUrl} class="ml-1 text-xs text-green-500 row">
-						Linktree
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							class="size-5"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-							/>
-						</svg>
-					</a>
+						<!-- 🌐 External Link -->
+						<a href={bandObject.instagram?.externalUrl} class="ml-1 text-xs text-green-500 row">
+							Linktree
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="size-5"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+								/>
+							</svg>
+						</a>
+					{/if}
 				</button>
 			{/each}
 		</div>
