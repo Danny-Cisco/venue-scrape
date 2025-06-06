dcdb96982b646434a6d6196d9d1356accfa735cc
Danny Cisco <gumballelectronics@gmail.com>
2025-06-06 20:18:31 +1000

    moved starRating to a component

--- src/lib/components/modals/GigModal.svelte
+++ src/lib/components/modals/GigModal.svelte
@@ -5,12 +5,12 @@
 
 	import { marked } from 'marked';
 
-	import { getStarCount } from '$lib/utils/stars.js';
 	import { weserv, imgHaste, photon } from '$lib/utils/image.js';
 
 	import RetryingImage from '$lib/components/ui/RetryingImage.svelte';
 
 	import DateTextMinimal from '$lib/components/ui/DateTextMinimal.svelte';
+	import StarRating from '../ui/StarRating.svelte';
 
 	import { onMount } from 'svelte';
 
@@ -75,9 +75,9 @@
 			<p>Featuring:</p>
 			{#each gig.bandObjects as bandObject, i}
 				{#if i == 0}
-					<li class="ml-2 text-2xl">{bandObject.bandname}</li>
+					<li class="ml-2 text-2xl row">{bandObject.bandname} <StarRating {bandObject} /></li>
 				{:else}
-					<li class="ml-2">{bandObject.bandname}</li>
+					<li class="ml-2 row">{bandObject.bandname} <StarRating {bandObject} /></li>
 				{/if}
 			{/each}
 		</ul>
@@ -161,43 +161,7 @@
 											</h2>
 										</div>
 										<!-- Star Rating -->
-										<div class="flex items-center">
-											<div class="flex">
-												{#each Array(5) as _, i}
-													{#if i < getStarCount(bandObject.instagram.followersCount)}
-														<!-- Filled Star -->
-														<svg
-															xmlns="http://www.w3.org/2000/svg"
-															viewBox="0 0 24 24"
-															fill="currentColor"
-															class="text-yellow-400 size-5"
-														>
-															<path
-																fill-rule="evenodd"
-																d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.006Z"
-																clip-rule="evenodd"
-															/>
-														</svg>
-													{:else}
-														<!-- Outline Star -->
-														<svg
-															xmlns="http://www.w3.org/2000/svg"
-															fill="none"
-															viewBox="0 0 24 24"
-															stroke-width="1.5"
-															stroke="currentColor"
-															class="text-gray-300 size-5"
-														>
-															<path
-																stroke-linecap="round"
-																stroke-linejoin="round"
-																d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
-															/>
-														</svg>
-													{/if}
-												{/each}
-											</div>
-										</div>
+										<StarRating {bandObject} />
 
 										<!-- Instagram Info Section -->
 										<div class="">
