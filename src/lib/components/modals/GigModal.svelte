a65943ccfdc9d9f05d8bc09f56355656ba1bc2f7
Danny Cisco <gumballelectronics@gmail.com>
2025-06-07 18:16:52 +1000

    moved the BandCard to a cards folder

--- src/lib/components/modals/GigModal.svelte
+++ src/lib/components/modals/GigModal.svelte
@@ -5,12 +5,11 @@
 
 	import { marked } from 'marked';
 
-	import { weserv, imgHaste, photon } from '$lib/utils/image.js';
-
 	import RetryingImage from '$lib/components/ui/RetryingImage.svelte';
 
 	import DateTextMinimal from '$lib/components/ui/DateTextMinimal.svelte';
 	import StarRatingBarColor from '../ui/StarRatingBarColor.svelte';
+	import BandCard from '../cards/BandCard.svelte';
 
 	import { onMount } from 'svelte';
 
@@ -57,10 +56,6 @@
 		console.log('🚀 ~ formatText ~ data:', data);
 		formatting = false;
 		return data.answer;
-	}
-
-	function openBandModal(band) {
-		console.log('band clicked 👉 ', band);
 	}
 </script>
 
@@ -152,145 +147,8 @@
 						class="flex flex-col items-center flex-1 w-full pb-10 space-y-4 overflow-hidden overflow-y-auto"
 					>
 						{#each gig.bandObjects as bandObject (bandObject.bandname)}
-							<!-- Card container - this is the main change -->
-							<button
-								class="w-full max-w-md p-3 mx-4 text-gray-800 transition-shadow duration-300 ease-in-out bg-white rounded-sm shadow-lg hover:shadow-xl focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-75"
-								on:click={() => openBandModal(bandObject.bandname)}
-							>
-								<div class="flex min-w-full">
-									<!-- Instagram Profile Picture via Weserv -->
-									<div
-										class="bg-gradient-to-br from-gray-500/5 gap-4 to-black/5 center font-sans text-xs text-gray-400 font-extralight min-w-[150px] max-w-[150px] rounded-sm overflow-hidden min-h-[150px] max-h-[150px]"
-									>
-										{#if bandObject.instagram?.profilePicUrl}
-											<img
-												src={weserv(bandObject.instagram.profilePicUrl)}
-												alt="_profile pic"
-												fallback="/fallback-avatar.png"
-											/>
-										{/if}
-									</div>
-									<div class="flex flex-col w-full ml-4">
-										<!-- Top section: Dot and Band Name -->
-										<div class="">
-											<!-- <div class="w-3 h-3 mr-3 bg-blue-500 rounded-full shrink-0"></div> -->
-											<h2
-												class="mb-0 font-sans text-2xl font-black text-black capitalize text-start"
-											>
-												{bandObject.bandname}
-											</h2>
-											{#if bandObject.instagram}
-												<p>
-													<a
-														href={bandObject.instagram?.url}
-														target="_blank"
-														rel="noopener noreferrer"
-														class="flex text-xs text-gray-400 font-extralight hover:underline"
-														on:click|stopPropagation
-														>@{bandObject.instagram?.username || bandObject.instagram}
-													</a>
-												</p>
-											{/if}
-										</div>
-										<!-- Star Rating -->
-										<div class="flex mt-3 mb-3">
-											<StarRatingBarColor {bandObject} />
-										</div>
-
-										<!-- <div class="flex-1"></div> -->
-
-										<!-- Instagram Info Section -->
-										<div class="grid w-full h-full grid-cols-2">
-											<div>
-												{#if bandObject.instagram}
-													<div class="flex flex-col items-start space-y-1">
-														{#if bandObject.instagram.followersCount}
-															<div class="flex gap-2 text-sm text-blue-500">
-																{(bandObject.instagram.followersCount / 1000).toFixed(1)}k
-																<div class="font-sans text-sm font-bold">Followers</div>
-															</div>
-														{/if}
-														{#if bandObject.instagram.postsCount}
-															<div class="flex gap-2 text-sm text-blue-500">
-																{bandObject.instagram.postsCount}
-																<div class="font-sans text-sm font-bold">Posts</div>
-															</div>
-														{/if}
-													</div>
-												{:else}
-													<div class="text-xs italic text-gray-500">No Instagram profile</div>
-												{/if}
-											</div>
-											<!-- links section -->
-
-											{#if bandObject.instagram}
-												<div
-													class="flex flex-col items-end h-full pt-3 mt-auto space-y-1 border-t-0 border-gray-200"
-												>
-													<div class="flex-1"></div>
-													<div class="">
-														<!-- InstagramLink -->
-
-														<a
-															href={bandObject.instagram.url}
-															target="_blank"
-															rel="noopener noreferrer"
-															class="flex text-xs text-blue-600 row hover:underline"
-															on:click|stopPropagation
-															>Instagram
-															<svg
-																xmlns="http://www.w3.org/2000/svg"
-																fill="none"
-																viewBox="0 0 24 24"
-																stroke-width="1.5"
-																stroke="currentColor"
-																class="ml-1 size-4"
-															>
-																<path
-																	stroke-linecap="round"
-																	stroke-linejoin="round"
-																	d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-																/>
-															</svg></a
-														>
-													</div>
-													<!-- External Link -->
-													{#if bandObject.instagram.externalUrl}
-														<a
-															href={bandObject.instagram.externalUrl}
-															target="_blank"
-															rel="noopener noreferrer"
-															class="flex items-center text-xs text-green-600 row hover:text-green-700"
-															on:click|stopPropagation
-														>
-															Linktree <svg
-																xmlns="http://www.w3.org/2000/svg"
-																fill="none"
-																viewBox="0 0 24 24"
-																stroke-width="1.5"
-																stroke="currentColor"
-																class="ml-1 size-4"
-															>
-																<path
-																	stroke-linecap="round"
-																	stroke-linejoin="round"
-																	d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-																/>
-															</svg>
-														</a>
-													{/if}
-												</div>
-											{/if}
-										</div>
-									</div>
-								</div>
-								<!-- Biography section -->
-								<div>
-									<p class=" font-sans min-h-[3.5rem] pt-2 text-sm text-left text-black">
-										{bandObject.instagram?.biography || ''}
-									</p>
-								</div>
-							</button>
+							<!-- BAND CARD SECTION -->
+							<BandCard {bandObject} />
 						{/each}
 					</div>
 				</div>
