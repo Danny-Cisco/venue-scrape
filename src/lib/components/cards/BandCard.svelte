e6038e174fd772099741c7702cfe81e0d07b697e
Danny Cisco <gumballelectronics@gmail.com>
2025-06-09 14:27:45 +1000

    allignment

--- src/lib/components/cards/BandCard.svelte
+++ src/lib/components/cards/BandCard.svelte
@@ -82,7 +82,7 @@
 				{/if}
 			{/key}
 		</div>
-		<div class="flex flex-col w-full h-full ml-4">
+		<div class="flex flex-col w-full h-[150px] ml-4">
 			<!-- Top section: Dot and Band Name -->
 			<div class="flex flex-col justify-between w-full h-full max-w-full">
 				<!-- <div class="w-3 h-3 mr-3 bg-blue-500 rounded-full shrink-0"></div> -->
@@ -105,95 +105,95 @@
 						</p>
 					{/if}
 				</div>
-				<div class="grid items-stretch w-full grid-cols-2">
-					<div class="flex flex-col justify-between">
-						<!-- Star Rating -->
-						<div class="flex mt-3 mb-3">
-							<StarRatingBarColor {bandObject} />
-						</div>
-
-						<!-- <div class="flex-1"></div> -->
-
-						<!-- Instagram Info Section -->
-						<div class="block">
-							{#if bandObject.instagram}
-								{#if bandObject.instagram.followersCount}
-									<div class="flex items-end gap-2 text-sm text-blue-500">
-										{(bandObject.instagram.followersCount / 1000).toFixed(1)}k
-										<div class="font-sans text-sm font-bold">Followers</div>
-									</div>
-								{/if}
-								{#if bandObject.instagram.postsCount}
-									<div class="flex items-end gap-2 text-sm text-blue-500">
-										{bandObject.instagram.postsCount}
-										<div class="font-sans text-sm font-bold">Posts</div>
-									</div>
-								{/if}
-							{:else}
-								<div class="text-xs italic text-gray-500">No Instagram profile</div>
-							{/if}
-						</div>
+			</div>
+			<div class="grid items-stretch w-full grid-cols-2">
+				<div class="flex flex-col justify-between">
+					<!-- Star Rating -->
+					<div class="flex mt-3 mb-3">
+						<StarRatingBarColor {bandObject} />
 					</div>
 
-					<div
-						class="flex flex-col items-end h-full pt-3 mt-auto space-y-1 border-t-0 border-gray-200"
-					>
-						<!-- links section -->
-						{#if bandObject.instagram}
-							<div class="flex-1"></div>
-							<div class="">
-								<!-- InstagramLink -->
+					<!-- <div class="flex-1"></div> -->
 
-								<a
-									href={bandObject.instagram.url}
-									target="_blank"
-									rel="noopener noreferrer"
-									class="flex text-xs text-blue-600 row hover:underline"
-									on:click|stopPropagation
-									>Instagram
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										stroke-width="1.5"
-										stroke="currentColor"
-										class="ml-1 size-4"
-									>
-										<path
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-										/>
-									</svg></a
-								>
-							</div>
-							<!-- External Link -->
-							{#if bandObject.instagram.externalUrl}
-								<a
-									href={bandObject.instagram.externalUrl}
-									target="_blank"
-									rel="noopener noreferrer"
-									class="flex items-center text-xs text-green-600 row hover:text-green-700"
-									on:click|stopPropagation
-								>
-									Linktree <svg
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										stroke-width="1.5"
-										stroke="currentColor"
-										class="ml-1 size-4"
-									>
-										<path
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-										/>
-									</svg>
-								</a>
+					<!-- Instagram Info Section -->
+					<div class="block">
+						{#if bandObject.instagram}
+							{#if bandObject.instagram.followersCount}
+								<div class="flex items-end gap-2 text-sm text-blue-500">
+									{(bandObject.instagram.followersCount / 1000).toFixed(1)}k
+									<div class="font-sans text-sm font-bold">Followers</div>
+								</div>
 							{/if}
+							{#if bandObject.instagram.postsCount}
+								<div class="flex items-end gap-2 text-sm text-blue-500">
+									{bandObject.instagram.postsCount}
+									<div class="font-sans text-sm font-bold">Posts</div>
+								</div>
+							{/if}
+						{:else}
+							<div class="text-xs italic text-gray-500">No Instagram profile</div>
 						{/if}
 					</div>
+				</div>
+
+				<div
+					class="flex flex-col items-end h-full pt-3 mt-auto space-y-1 border-t-0 border-gray-200"
+				>
+					<!-- links section -->
+					{#if bandObject.instagram}
+						<div class="flex-1"></div>
+						<div class="">
+							<!-- InstagramLink -->
+
+							<a
+								href={bandObject.instagram.url}
+								target="_blank"
+								rel="noopener noreferrer"
+								class="flex text-xs text-blue-600 row hover:underline"
+								on:click|stopPropagation
+								>Instagram
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									class="ml-1 size-4"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+									/>
+								</svg></a
+							>
+						</div>
+						<!-- External Link -->
+						{#if bandObject.instagram.externalUrl}
+							<a
+								href={bandObject.instagram.externalUrl}
+								target="_blank"
+								rel="noopener noreferrer"
+								class="flex items-center text-xs text-green-600 row hover:text-green-700"
+								on:click|stopPropagation
+							>
+								Linktree <svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									class="ml-1 size-4"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+									/>
+								</svg>
+							</a>
+						{/if}
+					{/if}
 				</div>
 			</div>
 		</div>
