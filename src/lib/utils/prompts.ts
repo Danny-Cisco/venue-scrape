ff28edf882944d65a877b805a7083b20927c0362
Danny Cisco <gumballelectronics@gmail.com>
2025-06-13 14:39:38 +1000

    prompt iseads notes

--- src/lib/utils/prompts.ts
+++ src/lib/utils/prompts.ts
@@ -34,6 +34,12 @@ export const genres = [
 	'Other'
 ];
 
+// PERSONAS???
+
+// INTENSITY??
+
+// VIBES??
+
 export const liveMusicCheck = `You are to act as a simple music classifier. with the prime function of weeding out gigs that are heavily relying on djs to fill the night rather than bands. You will be given a description of an event, and you are to determine if the event is PREDOMINANTLY a live music event. Some events may be predominantly DJs, so these events will therefore NOT be live music events.  The output must be in the following json format. {"thinking": <please use this field to explain your thinking process, beginning with "So, I have been asked to determine if this gig is PREDOMINANTLY a live music event, billed with bands and/or solo artists, not djs.>, "liveMusic": [ <here is where you place the boolean TRUE or FALSE> ]} . Please dont say anything else or you will spoil the program. please dont include backticks or anything else like the word json. just provide the json, and do your thinking inside the provided field.  `;
 
 export const genreClassifier = `You are to act as a simple music genre classifier. You will be provided with all the information I have regarding a music gig. This may include: The description of the event. The tags from the ticketing site. The bios from the band's instagram pages. The content and tags from the band's recent instagram posts. Taking into consideration all the provided information, your job is to determine which of the following music genres best describe the event. the exact genres you are to use are as follows ${genres}. Please make certain to use ONLY these specific genres with these specific spellings. For example, the genre "Ska & Reggae" cannot be split to just Ska or just Reggae. Dont be afraid to add a genre that is a secondary genre, as it is not uncommon to describe a gig as a combination of two genres, and my system is designed to handle such cases. The output must be in the following json format. {"thinking": <please use this field to explain your thinking process, beginning with "So, I have been asked to classify this content as an array containing a single musical genre or a few genres>, "genres": [ <here is where you place the genre or genres> ]} . Please dont say anything else or you will spoil the program. please dont include backticks or anything else like the word json. just provide the json, and do your thinking inside the provided field.  `;
