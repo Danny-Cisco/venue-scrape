9f13bebb1e91b622b231e40ab86b71a8e0f1212b
Danny Cisco <gumballelectronics@gmail.com>
2025-06-16 15:36:10 +1000

    more discrete single dots op;tions

--- src/lib/components/outputs/UpsetPlot.svelte
+++ src/lib/components/outputs/UpsetPlot.svelte
@@ -528,10 +528,10 @@
 		on:click={() => {
 			showSingleDotsSelection = !showSingleDotsSelection;
 		}}
-		class="flex items-center gap-1 text-xs text-gray-400 hover:text-gray-200"
+		class="flex items-center gap-1 text-xs font-thin text-gray-400 font-body hover:text-gray-200"
 		transition:slide={{ axis: 'x' }}
 	>
-		<svg
+		<!-- <svg
 			xmlns="http://www.w3.org/2000/svg"
 			fill="none"
 			viewBox="0 0 24 24"
@@ -544,20 +544,24 @@
 				stroke-linejoin="round"
 				d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z"
 			/>
-		</svg>
-		Single Dots
+		</svg> -->
+		Options?
 	</button>
 	{#if showSingleDotsSelection}
-		<select
-			id="solo-mode-select"
-			class="p-1 text-xs text-white bg-gray-800 border border-gray-700 rounded shadow-sm focus:ring-blue-500 focus:border-blue-500/50"
-			bind:value={soloSetMode}
-			transition:slide={{ axis: 'x' }}
-		>
-			<option value="outersect">Exclusive</option>
-			<option value="all">Inclusive</option>
-			<option value="none">None</option>
-		</select>
+		<div transition:slide={{ axis: 'x' }}>
+			<div class="whitespace-nowrap">
+				Single Dots -
+				<select
+					id="solo-mode-select"
+					class="p-1 text-xs text-white bg-gray-800 border border-gray-700 rounded shadow-sm focus:ring-blue-500 focus:border-blue-500/50"
+					bind:value={soloSetMode}
+				>
+					<option value="outersect">Exclusive</option>
+					<option value="all">Inclusive</option>
+					<option value="none">None</option>
+				</select>
+			</div>
+		</div>
 	{/if}
 </div>
 <!-- Container div -->
