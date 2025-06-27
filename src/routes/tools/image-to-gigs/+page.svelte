a8ce2a63ef5ae40d008a842a5ee876da23b72871
Danny Cisco <gumballelectronics@gmail.com>
2025-06-27 22:42:38 +1000

    finished the image to gigs test page with clip board and pretty formatting

--- src/routes/tools/image-to-gigs/+page.svelte
+++ src/routes/tools/image-to-gigs/+page.svelte
@@ -2,11 +2,14 @@
 	import { slide } from 'svelte/transition';
 	import { imageToGigsJSON } from '$lib/utils/prompts';
 	import { nowForChat } from '$lib/utils/date';
+	import CopyClipboard from '$lib/components/ui/CopyClipboard.svelte';
 
+	let question = '';
 	let systemPrompt = `${imageToGigsJSON}. To help you infer the startDate when no year is given allow me to let you know that todays data is ${nowForChat()}. Good Luck. Be thourough! `;
 	let imageFile = null;
 	let resizedImageDataUrl = '';
 	let responseText = '';
+	let prettyResponse = '';
 	let loading = false;
 
 	async function resizeImageNoAspect(file, size = 1024, quality = 0.1) {
@@ -43,6 +46,7 @@
 
 		const payload = {
 			systemPrompt,
+			question,
 			imageBase64: resizedImageDataUrl
 		};
 
@@ -56,6 +60,7 @@
 			});
 			const data = await response.json();
 			responseText = data.answer;
+			prettyResponse = JSON.stringify(JSON.parse(responseText), null, 2);
 		} catch (err) {
 			console.error('Failed to send image prompt', err);
 			responseText = 'Error sending image';
@@ -72,15 +77,32 @@
 	}
 </script>
 
-<div class="gap-4 page-center">
-	<h1 class="text-4xl">OpenAI Vision Bot</h1>
+<div class="flex flex-col gap-4 overflow-y-auto">
+	<h1 class="text-4xl font-fancy2">Image to Gigs</h1>
 
 	<p class="flex w-full">System Prompt:</p>
-	<textarea id="systemPrompt" class="w-full h-[150px]" bind:value={systemPrompt} />
+	<textarea id="systemPrompt" class="w-full h-[150px] font-sans" bind:value={systemPrompt} />
+	<p class="flex w-full">Prompt:</p>
+
+	<textarea
+		name="question"
+		id="question"
+		class="w-full font-sans"
+		on:keydown={(e) => {
+			if (e.key === 'Enter' && e.metaKey) sendQuestion();
+		}}
+		bind:value={question}
+	/>
 
 	<p class="flex w-full mt-4">Upload Image:</p>
-	<input type="file" accept="image/*" on:change={handleFileChange} />
+	<input
+		class="p-4 rounded-full border-[1px] flex w-full justify-around bg-white/30 border-white border-dashed hover:border-pink-500"
+		type="file"
+		accept="image/*"
+		on:change={handleFileChange}
+	/>
 
+	<button on:click={sendImagePrompt} class="mt-4 btn">Send Image to OpenAI</button>
 	{#if resizedImageDataUrl}
 		<p class="mt-4">Preview (stretched to 1024x1024 JPEG):</p>
 		<img
@@ -89,19 +111,20 @@
 			class="mt-2 rounded shadow w-[1024px] h-[1024px]"
 		/>
 	{/if}
-
-	<button on:click={sendImagePrompt} class="mt-4 btn">Send Image to OpenAI</button>
-
 	{#if loading}
 		<div class="mt-4 row">
 			loading <img src="/pac-man.svg" alt="Loading" class="size-10" transition:slide />
 		</div>
 	{/if}
 
-	{#if responseText}
-		<p class="flex w-full mt-4">Answer:</p>
-		<div class="rounded bg-white/15">
-			<p class="p-4 rounded">{responseText}</p>
+	{#if prettyResponse}
+		<p class="w-full mt-4">Answer:</p>
+		<div class="relative">
+			<div class="absolute top-2 right-2">
+				<CopyClipboard text={prettyResponse} />
+			</div>
+
+			<pre class="max-w-full p-4 whitespace-pre-wrap rounded bg-white/5">{prettyResponse}</pre>
 		</div>
 	{/if}
 </div>
