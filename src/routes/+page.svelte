6a945ea9500d2a9556089acecd519f9fec4862c2
Danny Cisco <gumballelectronics@gmail.com>
2025-07-21 17:07:33 +1000

    fixed title page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,17 +7,15 @@
 </script>
 
 <div class="page-center" in:fade>
-	<h1 class="text-center text-transparent uppercase font-white text-8xl font-fancy2">
-		Music Naarm
-	</h1>
+	<h1 class="font-black text-center text-white uppercase text-8xl font-fancy2">Music Naarm</h1>
 	<p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
 		Book better with data.
 	</p>
-	<!-- <a
-		href="/learn-more"
+	<a
+		href="/dashboards/genre-chart"
 		class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
 	>
-		Learn more
+		Go to Genre Chart
 		<svg
 			class="w-3.5 h-3.5 ms-2 rtl:rotate-180"
 			aria-hidden="true"
@@ -33,5 +31,5 @@
 				d="M1 5h12m0 0L9 1m4 4L9 9"
 			/>
 		</svg>
-	</a> -->
+	</a>
 </div>
