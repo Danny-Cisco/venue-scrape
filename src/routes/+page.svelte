72f7fbb0eef4ae5a1f9b5867371604850c672ea0
Danny Cisco <gumballelectronics@gmail.com>
2025-08-22 22:19:04 +1000

    correct text... image with no text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,17 +5,22 @@
 	onMount(() => {});
 </script>
 
-<div class="absolute inset-0 bg-black">
-	<div class="page-center" in:fade>
-		<div class="flex flex-col items-stretch items-start justify-start">
-			<div class="relative">
-				<img
-					src="http://musicnaarm.com/images/musicnaarm-poster.png"
-					alt="colorful graphic of electricity moving between 4 coloured rectangles"
-					class="object-cover w-full mx-auto mb-12"
-				/>
+<div class="page-center" in:fade>
+	<div class="absolute inset-0 h-full bg-black">
+		<div class="flex flex-col items-stretch items-start justify-start max-w-4xl mx-auto">
+			<img
+				src="http://musicnaarm.com/images/musicnaarm-image.png"
+				alt="colorful graphic of electricity moving between 4 coloured rectangles"
+				class="object-cover w-full mx-auto mt-20 mb-4"
+			/>
+			<div class="flex flex-wrap justify-between">
+				<h1 class="font-black text-left uppercase text-8xl text-cream sm:text-9xl font-fancy2">
+					Music
+				</h1>
+				<h1 class="font-black text-left uppercase text-8xl text-cream sm:text-9xl font-fancy2">
+					Naarm
+				</h1>
 			</div>
-
 			<a
 				href="/dashboards/genre-chart"
 				class="inline-flex items-center justify-center w-full px-5 py-4 text-base font-medium text-center text-white rounded-lg cta-button bg-naarmpink-500 hover:bg-purple-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
@@ -37,21 +42,18 @@
 					/>
 				</svg>
 			</a>
-			<p class="w-full mt-10 mr-auto font-sans font-thin text-center text-7xl text-gray-500/50">
-				FIND YOUR CROWD.
+		</div>
+		<div class="w-full pb-20 mx-auto mt-10 text-center bg-black">
+			<p class="font-sans text-lg text-gray-300">
+				Want early access to the beta?
+				<a
+					href="https://musicnaarm.com"
+					class="underline transition-colors duration-200 text-naarmpink-500 hover:text-purple-400"
+				>
+					Join our mailing list
+				</a>
+				to sign up!
 			</p>
-			<div class="w-full mt-8 text-center">
-				<p class="font-sans text-lg text-gray-300">
-					Want early access to the beta?
-					<a
-						href="https://musicnaarm.com"
-						class="underline transition-colors duration-200 text-naarmpink-500 hover:text-purple-400"
-					>
-						Join our mailing list
-					</a>
-					to sign up!
-				</p>
-			</div>
 		</div>
 	</div>
 </div>
